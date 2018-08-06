488d76445794dc13fb39075fe25aa6b5e63d0e5b
Miguel Angel Espitia <miguelespitia094@gmail.com>
2018-08-06 21:50:56 +0000

    f

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-song = {
-	"artist" : "Kendrick Lamar",
-	"song_name" : "God’s Plan"
-}
+import webapp2
+from random import shuffle
+import jinja2
+import os
+
+#libraries for APIs
+from google.appengine.api import urlfetch
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+    
+class AboutPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/about.html')
+        self.response.write(about_template.render())
+
+class ContactPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/index.html')
+        self.response.write(about_template.render())
+   
+    def post(self):
+        isError = False
+        if(isError):
+            self.response.write("Error dummy!")
+        else:
+            self.redirect("/")
+
+app = webapp2.WSGIApplication([
+    ('/', AboutPage),
+    ('/contact', ContactPage),
+], debug=True)
