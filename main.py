f9b30a0da81f66e6205144ba43de3cd365fdaabf
Areli Agudo <arelibagudo@student.hartnell.edu>
2018-08-07 16:20:41 +0000

    commit

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-
-=======
 import webapp2
 from random import shuffle
 import jinja2
 import os
 
+#libraries for APIs
+from google.appengine.api import urlfetch
 
 #libraries for APIs
 from google.appengine.api import urlfetch
 import json
-
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -23,16 +22,19 @@ class AboutPage(webapp2.RequestHandler):
         self.response.write(about_template.render())
    
 
-
 class ContactPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(about_template.render())
-
+        
+    def post(self):
+        isError = False
+        if(isError):
+            self.response.write("Error dummy!")
+        else:
+            self.redirect("/")
 
 app = webapp2.WSGIApplication([
     ('/', AboutPage),
     ('/contact', ContactPage),
 ], debug=True)
-
-
