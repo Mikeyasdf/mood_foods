40ad79164aa6a07a9e1e79288a2dd8629464fda0
Areli Agudo <arelibagudo@student.hartnell.edu>
2018-08-07 18:14:51 +0000

    commit

--- main.py
+++ main.py
@@ -14,7 +14,18 @@ the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-    
+def button_choices(button_choice):
+    if button_choice == 'Happy':
+        url = 'https://upload.wikimedia.org/wikipedia/commons/4/47/StateLibQld_1_100348.jpg'
+    elif button_choice == 'Sad':
+        url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinusPaulingGraduation1922.jpg'
+    elif button_choice == 'Angry':
+        url = 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg'
+    elif button_choice == 'Nervous':
+        url = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Typing_computer_screen_reflection.jpg'
+    elif button_choice == 'Excited':
+        url = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Typing_computer_screen_reflection.jpg'
+    return url
     
 class AboutPage(webapp2.RequestHandler):
     def get(self):
