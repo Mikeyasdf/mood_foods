043ba37f7a59e2ae67b568c3efde7c8726561a1a
Roober Cruz <roober.cruz@gmail.com>
2018-08-07 17:22:30 +0000

    I can make buttons redirect. Does any1 know how to get rid of the 405 post request(or get) method  not allowed?

--- main.py
+++ main.py
@@ -20,18 +20,19 @@ class AboutPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/about.html')
         self.response.write(about_template.render())
-   
+
+#add <form action="/page_2" method="post"> to make buttons do stuff
+
+
+class Page_2(webapp2.RequestHandler):
+
     def post(self):
+        about_template = the_jinja_env.get_template('templates/page_2.html')
         isError = False
         if(isError):
             self.redirect("/page_2")
         else:
-            self.redirect("/") 
-
-class Page_2(webapp2.RequestHandler):
-    def get(self):
-        about_template = the_jinja_env.get_template('templates/index.html')
-        self.response.write(about_template.render())
+            self.redirect("/page_2")     
         
 
 app = webapp2.WSGIApplication([
