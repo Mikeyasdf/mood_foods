9bf2002a90fb7a65ebf80a0a5bf2b093d395cdae
Areli Agudo <arelibagudo@student.hartnell.edu>
2018-08-08 16:41:08 +0000

    commit

--- main.py
+++ main.py
@@ -16,17 +16,6 @@ the_jinja_env = jinja2.Environment(
 class AboutPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/about.html')
-<<<<<<< HEAD
-        self.response.write(about_template.render()) 
-class Page_2(webapp2.RequestHandler):
-
-    def post(self):
-        about_template = the_jinja_env.get_template('templates/page_2.html')
-        if True:
-            self.redirect("/page_2")
-        else:
-            self.redirect("/") 
-=======
         self.response.write(about_template.render())
         
 class Angry(webapp2.RequestHandler):
@@ -34,9 +23,26 @@ class Angry(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/Angry.html')
         self.response.write(about_template.render())
->>>>>>> 0c05bc94757d95d9ddfbf2f0f2cc2a39ad5e7051
+class Romantic(webapp2.RequestHandler):
 
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/Romantic.html')
+        self.response.write(about_template.render())
+
+'''
+class Page_2(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/index.html')
+        self.response.write(about_template.render())
+        self.redirect("/page_2")
+    def post(self):
+        if True:
+            self.response.write("Error dummy!")
+        else:
+            self.redirect("/")
+'''
 app = webapp2.WSGIApplication([
     ('/', AboutPage),
     ('/Angry', Angry),
+    ('/Romantic', Romantic)
 ], debug=True)
